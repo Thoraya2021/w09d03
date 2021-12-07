15b0da1dc413b627de0fe0049ee67243fe2de09e
Thoraya2021 <https://github.com/Thoraya2021/myFirstRepo>
2021-12-08 02:34:14 +0300

    tasksreducers

--- tasks/src/reducers/index.js
+++ tasks/src/reducers/index.js
@@ -2,7 +2,7 @@ import {createStore,combineReducers} from 'redux';
 import { composeWithDevTools } from "redux-devtools-extension";
 import signIn from './login';
 
-//all components what i will use should define here
+//all components what i will use should define here 
 const reducers =combineReducers ({signIn});
 
 const store=()=>{
---
+++ tasks/src/reducers/task.js
@@ -0,0 +1,36 @@
+const initialState = {
+
+    tasks: [],
+  };
+  //name of reducer take function with 2 parameters state and action
+  const tasksReducer = (state = initialState, action) => {
+    const { type, payload } = action;
+  
+    switch (type) {
+      case "GET_ALL_TASKS":
+        return payload; //here will return for me tasks from payload
+  
+      case "ADD_NEW_TASK": 
+    const tasks = [...state.tasks, payload]; //here i take old state and i will change add elements 
+        return { tasks }; 
+  
+      default:
+        return state;
+    }
+  };
+  
+  export default tasksReducer;
+  
+  export const getAllTasks = (data) => {
+    return {
+      type: "GET_ALL_TASKS",
+      payload: { tasks: data },
+    };
+  };
+  
+  export const addNewTasks = (data) => {
+    return {
+      type: "ADD_NEW_TASK",
+      payload: data,
+    };
+  };
